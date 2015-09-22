7e77252431432f3f48c02c7bf3d66a14aaac088f
Tran Khanh <khanhth@evolableasia.vn>
2015-09-22 15:38:03 +0700

    Add login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h3>Trang Đăng Nhập</h3>
+		<form method="POST" action="authenticate.php">
+			<div>
+				<label for="id">ID:</label>
+				<input id="id" type="text" name="id" value="">
+			</div>
+
+			<div>
+				<label for="password">Password:</label>
+				<input id="password" type="password" name="password" value="">
+			</div>
+			<div>
+				<input type="submit" value="Đăng nhập">
+			</div>
+		</form>
+	</body>
+</html>
